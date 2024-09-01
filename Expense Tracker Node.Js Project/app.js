d6562bfff5b2d067e885970aeab82eae92b94e9f
puranrana080 <puransinghrana080@gmail.com>
2024-09-01 23:40:27 +0530

    Make the backend api of the login

--- Expense Tracker Node.Js Project/app.js
+++ Expense Tracker Node.Js Project/app.js
@@ -1,74 +1,71 @@
-const express=require('express')
-const path=require('path')
-const sequelize=require("./util/user")
-const User=require("./model/user")
-const  app=express()
+const express = require('express')
+const path = require('path')
+const sequelize = require("./util/user")
+const User = require("./model/user")
+const app = express()
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/',(req,res,next)=>{
-    res.sendFile(path.join(__dirname,"../public/index.html"))
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
-app.post('/user/register',async (req,res,next)=>{
-    try{
-    console.log(req.body)
+app.post('/user/register', async (req, res, next) => {
+    try {
+        console.log(req.body)
 
-    const existingUser=await User.findOne({
-        where:{
-            userEmail : req.body.userEmail
+        const existingUser = await User.findOne({
+            where: {
+                userEmail: req.body.userEmail
+            }
+        })
+        if (existingUser) {
+            return res.status(400).json({ message: "User Already exist, Try with new Email" })
         }
-    })
-    if(existingUser){
-        return res.status(400).json({message:"User Already exist, Try with new Email"})
-    }
 
-    const user= await User.create({
-        userName:req.body.userName,
-        userEmail:req.body.userEmail,
-        password:req.body.password
-    })
-    console.log(user)
-    res.status(200).json({newUser:user})
+        const user = await User.create({
+            userName: req.body.userName,
+            userEmail: req.body.userEmail,
+            password: req.body.password
+        })
+        console.log(user)
+        res.status(200).json({ newUser: user })
 
     }
-    catch(err){
-        console.log("User not Created",err)
+    catch (err) {
+        console.log("User not Created", err)
         res.status(500).json({ message: "Internal server error" });
     }
 })
 
 
-app.post("/user/login", async(req,res,next)=>{
-     try{
+app.post("/user/login", async (req, res, next) => {
+    try {
         console.log(req.body)
-        const userAvailable=await User.findOne({
-            where:{userEmail:req.body.email}
+        const userAvailable = await User.findOne({
+            where: { userEmail: req.body.email }
         })
-        if(userAvailable){
+        if (userAvailable) {
 
-            if(req.body.password===userAvailable.password){
+            if (req.body.password === userAvailable.password) {
                 console.log("login successful")
-                     res.status(200).json({message:"logged In"})
+                res.status(200).json({ message: "User Login Successful" })
             }
-            else{
+            else {
                 console.log("Password Wrong")
-                res.status(401).json({message:"password Wrong"})
+                res.status(401).json({ message: "User not authorized" })
             }
-
-
         }
-        else{
+        else {
             console.log("not logged in")
-            res.status(404).json({message:"need to signUp"})
+            res.status(404).json({ message: "User not found" })
         }
-
-     }
-     catch(error){
-res.status(500).json({message:"Internal error"})
-     }
+    }
+    catch (error) {
+        res.status(500).json({ message: "Internal error" })
+    }
 })
 
 
@@ -76,12 +73,12 @@ res.status(500).json({message:"Internal error"})
 
 
 sequelize.sync()
-.then(()=>{
-    app.listen(3000,()=>{
-        console.log("Listeneing to port 3000")
-    })
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("Listeneing to port 3000")
+        })
 
-})
-.catch(err=>{
-    console.log(err)
-})
+    })
+    .catch(err => {
+        console.log(err)
+    })
--- Expense Tracker Node.Js Project/public/index.js
+++ Expense Tracker Node.Js Project/public/index.js
@@ -1,44 +1,44 @@
-function registeringUser(event){
+function registeringUser(event) {
     event.preventDefault()
-    const newUser={
-        userName:event.target.name.value,
-        userEmail:event.target.email.value,
-        password:event.target.password.value
+    const newUser = {
+        userName: event.target.name.value,
+        userEmail: event.target.email.value,
+        password: event.target.password.value
     }
 
-    axios.post("http://localhost:3000/user/register",newUser)
-    .then(response=>{
-        console.log("User Added",response.data.newUser)
-    })
-    .catch(err=>{
-        const p=document.querySelector('p')
-        const exist=document.createElement('p')
-        exist.appendChild(document.createTextNode("User Already exist"))
-        p.appendChild(exist)
-    
-        console.log("something is wrong",err)
-    })
+    axios.post("http://localhost:3000/user/register", newUser)
+        .then(response => {
+            console.log("User Added", response.data.newUser)
+        })
+        .catch(err => {
+            const p = document.querySelector('p')
+            const exist = document.createElement('p')
+            exist.appendChild(document.createTextNode("User Already exist"))
+            p.appendChild(exist)
+
+            console.log("something is wrong", err)
+        })
 }
 
 
-function loginUser(event){
+function loginUser(event) {
     event.preventDefault()
-    const loginData={
-        email:event.target.email.value,
-        password:event.target.password.value
+    const loginData = {
+        email: event.target.email.value,
+        password: event.target.password.value
     }
-    axios.post("http://localhost:3000/user/login",loginData)
-    .then(response=>{
-        alert("Login successful")
-console.log("User Logged in",response.data.message)
-    })
-    .catch(err=>{
-        const p=document.querySelector('p')
-        const exist=document.createElement('p')
-        exist.appendChild(document.createTextNode("User Not found"))
-        p.appendChild(exist)
-    
-        console.log("Error in server",err)
-    })
+    axios.post("http://localhost:3000/user/login", loginData)
+        .then(response => {
+            alert("Login successful")
+            console.log("User Logged in", response.data.message)
+            const p = document.querySelector('#message')
+            p.innerHTML = ""
+        })
+        .catch(err => {
+            const p = document.querySelector('#message')
+            p.innerHTML = "User Not found"
+
+            console.log("Error in server", err)
+        })
 
 }
