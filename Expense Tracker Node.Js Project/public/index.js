7690d363bb16658d29900fb70e788421bc59fb2f
puranrana080 <puransinghrana080@gmail.com>
2024-09-09 00:56:18 +0530

    Report Generation Frontend  Download btn

--- Expense Tracker Node.Js Project/public/index.js
+++ Expense Tracker Node.Js Project/public/index.js
@@ -229,3 +229,26 @@ document.getElementById('rzp-button').onclick = async function (e) {
     })
 
 }
+
+
+
+
+function download(){
+    axios.get('http://localhost:3000/user/download', { headers: {"Authorization" : token} })
+    .then((response) => {
+        if(response.status === 201){
+            //the bcakend is essentially sending a download link
+            //  which if we open in browser, the file would download
+            var a = document.createElement("a");
+            a.href = response.data.fileUrl;
+            a.download = 'myexpense.csv';
+            a.click();
+        } else {
+            throw new Error(response.data.message)
+        }
+
+    })
+    .catch((err) => {
+        showError(err)
+    });
+}
